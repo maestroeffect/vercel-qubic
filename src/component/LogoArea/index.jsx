ae85b17fc9dc8b2dc0d1b66eb55e33aa6a6f72b9
Precious <pomokhaiye8@gmail.com>
2024-12-11 12:38:04 +0100

    logo update

--- src/component/LogoArea/index.jsx
+++ src/component/LogoArea/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ProtoTypes from "prop-types";
 import { Link } from "react-router-dom";
 import logo from "../../assets/img/logo.png";
+import logo2 from "../../assets/img/qubicweblogo.jpeg";
 import logoDark from "../../assets/img/logo-2.png";
 import tpBanner from "../../assets/img/banner/banner-1.png";
 import banner from "../../assets/img/banner/banner-9.jpg";
@@ -14,15 +15,13 @@ const LogoArea = ({ className, dark }) => {
           <div className="col-lg-4 align-self-center">
             <div className="logo">
               <Link to="/">
-                <img src={dark ? logoDark : logo} alt="logo" />
+                <img src={dark ? logoDark : logo2} alt="logo" />
               </Link>
             </div>
           </div>
           <div className="col-lg-8 align-self-center">
             <div className="banner1">
-              <Link to="#">
-                <img src={banner} alt="banner" />
-              </Link>
+              <Link to="#">{/* <img src={banner} alt="banner" /> */}</Link>
             </div>
           </div>
         </div>
