599caac69f5c5cdf25421c215f32dd218c3d7e53
Precious <pomokhaiye8@gmail.com>
2024-12-16 13:21:11 +0100

    recent

--- client/src/component/RssParser/index.jsx
+++ client/src/component/RssParser/index.jsx
@@ -7,7 +7,7 @@ const QubicwebFeed = () => {
   useEffect(() => {
     const fetchFeed = async () => {
       try {
-        const response = await fetch("http://localhost:5000/");
+        const response = await fetch("https://vercel-qubic-server.vercel.app/");
 
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
