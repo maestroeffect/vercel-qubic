2331b14e7d28bf136bcf2eadda43019a4f7a04c7
Precious <pomokhaiye8@gmail.com>
2024-12-21 02:34:32 +0100

    recent changes

--- client/src/component/RssParser/index.jsx
+++ client/src/component/RssParser/index.jsx
@@ -26,7 +26,9 @@ const QubicwebFeed = () => {
       setLoading(true); // Start loading
       try {
         // Fetch data from the server
-        const response = await fetch("http://localhost:5000/rss-feed");
+        // const response = await fetch("http://localhost:5000/rss-feed");
+
+        const response = await fetch("https://vercel-qubic-client.vercel.app/");
 
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
