b33f4a0e5959e80e896e014140e833d25155db77
Precious <pomokhaiye8@gmail.com>
2025-01-16 11:03:59 +0100

    solid

--- client/src/component/MainMenu/index.jsx
+++ client/src/component/MainMenu/index.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react';
-import { styled } from '@mui/material/styles';
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Switch from '@mui/material/Switch';
+import { useState } from "react";
+import { styled } from "@mui/material/styles";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Switch from "@mui/material/Switch";
 import FontAwesome from "../uiStyle/FontAwesome";
 import { Link, NavLink } from "react-router-dom";
 import SearchModal from "../SearchModal";
 import SidebarMenu from "../SidebarMenu";
 import ProtoTypes from "prop-types";
-import { useTheme } from '../../context/ThemeContext';
-
+import { useTheme } from "../../context/ThemeContext";
 
 const menus = [
   {
@@ -46,56 +45,54 @@ const menus = [
     link: "/feedDirectory",
     // icon: "angle-down",
   },
-
 ];
-
 
 const MaterialUISwitch = styled(Switch)(({ theme }) => ({
   width: 58,
   height: 30,
   padding: 7,
-  '& .MuiSwitch-switchBase': {
+  "& .MuiSwitch-switchBase": {
     margin: 1,
     padding: 0,
-    transform: 'translateX(6px)',
-    '&.Mui-checked': {
-      color: '#fff',
-      transform: 'translateX(22px)',
-      '& .MuiSwitch-thumb:before': {
+    transform: "translateX(6px)",
+    "&.Mui-checked": {
+      color: "#fff",
+      transform: "translateX(22px)",
+      "& .MuiSwitch-thumb:before": {
         backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
-          '#fff',
+          "#fff"
         )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
       },
-      '& + .MuiSwitch-track': {
+      "& + .MuiSwitch-track": {
         opacity: 1,
         backgroundColor: theme.palette.primary.main,
       },
     },
   },
-  '& .MuiSwitch-thumb': {
-    backgroundColor: theme.palette.mode === 'dark' ? '#003892' : '#001e3c',
+  "& .MuiSwitch-thumb": {
+    backgroundColor: theme.palette.mode === "dark" ? "#003892" : "#001e3c",
     width: 28,
     height: 28,
-    '&::before': {
+    "&::before": {
       content: "''",
-      position: 'absolute',
-      width: '100%',
-      height: '100%',
+      position: "absolute",
+      width: "100%",
+      height: "100%",
       left: 0,
       top: 0,
-      backgroundRepeat: 'no-repeat',
-      backgroundPosition: 'center',
+      backgroundRepeat: "no-repeat",
+      backgroundPosition: "center",
       backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
-        '#fff',
+        "#fff"
       )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
     },
     // ...theme.applyStyles('dark', {
     //   backgroundColor: '#003892',
     // }),
   },
-  '& .MuiSwitch-track': {
+  "& .MuiSwitch-track": {
     opacity: 1,
-    backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
+    backgroundColor: theme.palette.mode === "dark" ? "#8796A5" : "#aab4be",
     borderRadius: 20 / 2,
     // ...theme.applyStyles('dark', {
     //   backgroundColor: '#8796A5',
@@ -137,45 +134,45 @@ const MainMenu = ({ className }) => {
                     <ul className="nav navbar-nav" id="scroll">
                       {arr.length > 0
                         ? arr.map((item, i) => (
-                          <li
-                            key={i}
-                            className={`
+                            <li
+                              key={i}
+                              className={`
                                                   ${item.child ? "dropdown" : ""} 
                                                   nav-item`}
-                          >
-                            {item.child ? (
-                              <NavLink
-                                onClick={(e) => e.preventDefault()}
-                                to="/"
-                                className="menu-dropdown fw-bold"
-                                data-toggle="dropdown"
-                              >
-                                {item.linkText}
-                                <FontAwesome name={item.icon} />
-                              </NavLink>
-                            ) : (
-                              <NavLink
-                                to={item.link}
-                                className="menu-dropdown fw-bold"
-                                data-toggle="dropdown"
-                              >
-                                {item.linkText}{" "}
-                                <FontAwesome name={item.icon} />
-                              </NavLink>
-                            )}
-                            {item.child ? (
-                              <ul className="dropdown-menu" role="menu">
-                                {item.submenu.map((sub_item, i) => (
-                                  <li key={i}>
-                                    <NavLink to={sub_item.link}>
-                                      {sub_item.linkText}
-                                    </NavLink>
-                                  </li>
-                                ))}
-                              </ul>
-                            ) : null}
-                          </li>
-                        ))
+                            >
+                              {item.child ? (
+                                <NavLink
+                                  onClick={(e) => e.preventDefault()}
+                                  to="/"
+                                  className="menu-dropdown fw-bold"
+                                  data-toggle="dropdown"
+                                >
+                                  {item.linkText}
+                                  <FontAwesome name={item.icon} />
+                                </NavLink>
+                              ) : (
+                                <NavLink
+                                  to={item.link}
+                                  className="menu-dropdown fw-bold"
+                                  data-toggle="dropdown"
+                                >
+                                  {item.linkText}{" "}
+                                  <FontAwesome name={item.icon} />
+                                </NavLink>
+                              )}
+                              {item.child ? (
+                                <ul className="dropdown-menu" role="menu">
+                                  {item.submenu.map((sub_item, i) => (
+                                    <li key={i}>
+                                      <NavLink to={sub_item.link}>
+                                        {sub_item.linkText}
+                                      </NavLink>
+                                    </li>
+                                  ))}
+                                </ul>
+                              ) : null}
+                            </li>
+                          ))
                         : null}
                     </ul>
                   </div>
@@ -234,13 +231,13 @@ const MainMenu = ({ className }) => {
                             onChange={toggleDarkMode}
                           />
                         }
-                        label={darkMode ? 'Dark Mode' : 'Light Mode'}
+                        label={darkMode ? "Dark Mode" : "Light Mode"}
                         sx={{
-                          '& .MuiFormControlLabel-label': {
-                            fontSize: '0.85rem', // Adjust the font size here
-                            fontWeight: 'bold',
-                            marginLeft: '8px',  // Optional spacing adjustment
-                            fontFamily: 'Garamond, serif', // Ensure Garamond font is applied
+                          "& .MuiFormControlLabel-label": {
+                            fontSize: "0.85rem", // Adjust the font size here
+                            fontWeight: "bold",
+                            marginLeft: "8px", // Optional spacing adjustment
+                            fontFamily: "Garamond, serif", // Ensure Garamond font is applied
                           },
                         }}
                       />
